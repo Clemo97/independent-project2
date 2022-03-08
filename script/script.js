060bc125e8671b604828571c0efd10ef88998bab
Clemo97 <lumumbaclement@gmail.com>
2022-03-08 07:17:40 +0300

    day of the month vaildator.

--- script/script.js
+++ script/script.js
@@ -25,4 +25,23 @@ function getAkanName() {
             return true;
         }
     }
+
+    //validate day of birth to be between 1 and 28 for february or 1 and 31 for normal days
+    function dayValidator() {
+        if (monthOfBirth === 2 && Number(yearOfBirth) % 4 === 0) {
+            if (dayOfBirth > 28 || dayOfBirth < 1) {
+                return false;
+            } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+                return false;
+            } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else if (dayOfBirth < 1 || dayOfBirth > 31) {
+            return false;
+        } else {
+            return true;
+        }
+    }
     
